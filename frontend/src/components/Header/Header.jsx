4c74e9d72d87510482f791efed926d0a57230548
Nasyida <nikoloz2005@gmail.com>
2022-02-28 12:40:08 +0400

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="header__inner">
+            <div className="header__search">
+
+            </div>
+            <div className="header__nav">
+                
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
